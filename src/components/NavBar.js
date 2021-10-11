a6cf8b542ce7f4e9325f54c48a4c2f4fe5452b2c
Robertino <roberponzetti@gmail.com>
2021-10-11 10:11:06 -0300

    navbar & styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,24 +6,22 @@ const NavMenu = () => {
     return (
         <>
             <Navbar expand="lg" variant="dark" className="nav-color">
-                <Navbar.Brand href="/">
+                <Navbar.Brand href="/" className="brand-name">
                 <img
                     alt=""
-                    src="https://www.qbrobotics.com/wp-content/uploads/2018/03/sample-logo-470x235.png"
-                    //""img/ribel-logo.png""
-                    width="128"
-                    height="32"
+                    src="img/ribel-logo2.png"
+                    width="111"
+                    height="65"
                     className="d-inline-block align-top"
                 />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/inicio">Inicio</Nav.Link>
-                        <Nav.Link href="/productos">Productos</Nav.Link>
-                        <Nav.Link href="/conjuntos">Conjuntos</Nav.Link>
-                        <Nav.Link href="/contacto">Contacto</Nav.Link>
-                        <Nav.Link href="/faqs">Preguntas frecuentes</Nav.Link>
+                    <Nav className="mr-auto nav-links">
+                        <Nav.Link href="/productos" className="nav-link-item">Productos</Nav.Link>
+                        <Nav.Link href="/conjuntos" className="nav-link-item">Conjuntos</Nav.Link>
+                        <Nav.Link href="/contacto" className="nav-link-item">Contacto</Nav.Link>
+                        <Nav.Link href="/faqs" className="nav-link-item">Preguntas frecuentes</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
